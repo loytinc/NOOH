83a9741566a17380a92640e11685c43374d414ec
loyti <brice@loyti.com>
2017-09-14 11:31:21 -0700

    updates added core location

--- NOOH/ViewController.swift
+++ NOOH/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import CoreLocation
 
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        
+        if let manager = locationManager {
+            print("we have a managger")
+            manager.delegate = self
+            manager.requestAlwaysAuthorization()
+            manager.requestLocation()
+            manager.startUpdatingLocation()
+        } else {
+            print("no manager")
+        }
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //        print(locations)
+        for location in locations {
+            print(location.coordinate)
+            //            print(location.coordinate.latitude)
+            //            print(location.coordinate.longitude)
+        }
+    }
+    
+    
+    
+    
 }
 
